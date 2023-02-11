2e4e4e23c4921b2519c26c1e9aee1732d34834ed
keyem4251 <kisk.emura@gmail.com>
2023-02-11 13:05:59 +0900

    optimize algorithm

--- codes/1.two-sum.py
+++ codes/1.two-sum.py
@@ -8,11 +8,15 @@ from typing import List
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        reverse_dict = {}
         for i_index, i in enumerate(nums):
-            for j_index, j in enumerate(nums):
-                if i_index != j_index:
-                    if (i + j) == target:
-                        return [i_index, j_index]
+            j = target - i
+            if j in reverse_dict:
+                return [reverse_dict[j], i_index]
 
+            if i not in reverse_dict:
+                reverse_dict[i] = i_index
+
+   
 # @lc code=end
 
