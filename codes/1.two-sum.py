b7a86653c8cb66e9523df843bd46c8e651d808af
keyem4251 <kisk.emura@gmail.com>
2023-02-11 12:51:33 +0900

    brute force solution

--- codes/1.two-sum.py
+++ codes/1.two-sum.py
@@ -3,10 +3,16 @@
 #
 # [1] Two Sum
 #
+from typing import List
 
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        for i_index, i in enumerate(nums):
+            for j_index, j in enumerate(nums):
+                if i_index != j_index:
+                    if (i + j) == target:
+                        return [i_index, j_index]
+
 # @lc code=end
 
