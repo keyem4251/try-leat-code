516acf463b9d2a4945aacf4dfd780658ff1adb03
keyem4251 <kisk.emura@gmail.com>
2023-02-18 12:48:40 +0900

    pass default case

--- codes/20.valid-parentheses.py
+++ codes/20.valid-parentheses.py
@@ -7,6 +7,18 @@
 # @lc code=start
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+        l = []
+        for bracket in s:
+            if bracket in ["(", "[", "{"]:
+                l.append(bracket)
+            else:
+                if bracket == ")" and l[-1] == "(":
+                    l.pop(-1)
+                elif bracket == "]" and l[-1] == "[":
+                    l.pop(-1)
+                elif bracket == "}" and l[-1] == "{":
+                    l.pop(-1)
+
+        return True if not l else False
 # @lc code=end
 
