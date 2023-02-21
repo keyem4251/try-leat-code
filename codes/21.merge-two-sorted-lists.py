3c44973d463c24b7a282264af52a05e8ac62f7e4
keyem4251 <kisk.emura@gmail.com>
2023-02-21 20:44:22 +0900

    wrong answer

--- codes/21.merge-two-sorted-lists.py
+++ codes/21.merge-two-sorted-lists.py
@@ -14,6 +14,43 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        print(list1)
+        result = None
+        item1 = None
+        item2 = None
+
+        if not list1 and not list2:
+            return None
+
+        while True:
+            item1 = list1
+            while True:
+                item2 = list2
+
+                if item1.val >= item2.val:
+                    node2 = ListNode(val=item2.val)
+                    if result is None:
+                        result = node2
+                    else:
+                        result.next = node2
+                        node2 = None
+                else:
+                    break
+                
+                list2 = list2.next
+                if item2.next is None:
+                    break
+            
+            node1 = ListNode(val=item1.val)
+            if result is None:
+                result = node1
+            else:
+                result.next = node1
+                node1 = None
+            
+            list1 = list1.next
+            if item1.next is None:
+                break
+        
+        return result
 # @lc code=end
 
