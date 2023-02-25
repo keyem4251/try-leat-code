738e8a1b9981924611f40f601509466a9c50b3a9
keyem4251 <kisk.emura@gmail.com>
2023-02-25 13:05:48 +0900

    add solution

--- codes/26.remove-duplicates-from-sorted-array.py
+++ codes/26.remove-duplicates-from-sorted-array.py
@@ -8,6 +8,11 @@ from typing import List
 # @lc code=start
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        count_d = dict()
         
+        for n in nums:
+            count_d[n] = True
+
+        return len(count_d.keys())
 # @lc code=end
 
