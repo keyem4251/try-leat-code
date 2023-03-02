b715106a3cd0313acbe58dd7ef26c4c6fb839f13
keyem4251 <kisk.emura@gmail.com>
2023-03-02 21:16:09 +0900

    create solutin file

---
+++ codes/66.plus-one.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=66 lang=python3
+#
+# [66] Plus One
+#
+from typing import List
+
+# @lc code=start
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        pass        
+# @lc code=end
+
