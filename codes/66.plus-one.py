1150c75eea76a7f3ac8bac2d7eb8da67a5c47ace
keyem4251 <kisk.emura@gmail.com>
2023-03-05 17:53:54 +0900

    first solition

--- codes/66.plus-one.py
+++ codes/66.plus-one.py
@@ -8,6 +8,20 @@ from typing import List
 # @lc code=start
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        pass        
+        n = len(digits) - 1
+        digit = 0
+        for i in digits:
+            digit += i * (10 ** n)
+            n -= 1
+
+        plus_one = digit + 1
+        n = len(digits) - 1
+        result = []
+        for i in range(len(digits)):
+            result.append(int(plus_one / (10 ** n)))
+            plus_one = plus_one % (10 ** n)
+            n -= 1
+
+        return result
 # @lc code=end
 
