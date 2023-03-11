3dfecbc14a553e5c05d7a0f15f0d96417a0648fb
keyem4251 <kisk.emura@gmail.com>
2023-03-11 14:06:54 +0900

    answer

--- codes/70.climbing-stairs.py
+++ codes/70.climbing-stairs.py
@@ -7,6 +7,21 @@
 # @lc code=start
 class Solution:
     def climbStairs(self, n: int) -> int:
-        pass
+        d = dict()
+        d[0] = 1
+        d[1] = 1
+        if n in d:
+            return d[n]
+        
+        key1 = n - 1
+        key2 = n - 2
+        i = 2
+
+        while True:
+            if key1 in d and key2 in d:
+                return d[key1] + d[key2]
+
+            d[i] = d[i-1] + d[i-2]
+            i += 1
 # @lc code=end
 
