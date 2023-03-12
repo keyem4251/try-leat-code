69c508c7b8b4b8451bc1f2ad4f06fd29406cbd56
keyem4251 <kisk.emura@gmail.com>
2023-03-12 10:47:41 +0900

    create file

---
+++ codes/88.merge-sorted-array.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=88 lang=python3
+#
+# [88] Merge Sorted Array
+#
+from typing import List
+
+# @lc code=start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+# @lc code=end
+
