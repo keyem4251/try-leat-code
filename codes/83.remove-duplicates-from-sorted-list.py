5cb0620046bb76b9c35e2cc236b847251a99fbe4
keyem4251 <kisk.emura@gmail.com>
2023-03-12 10:46:37 +0900

    add solution

--- codes/83.remove-duplicates-from-sorted-list.py
+++ codes/83.remove-duplicates-from-sorted-list.py
@@ -4,6 +4,7 @@
 # [83] Remove Duplicates from Sorted List
 #
 from typing import Optional
+from collections import OrderedDict
 
 # @lc code=start
 # Definition for singly-linked list.
@@ -14,6 +15,23 @@ class ListNode:
 
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        d = OrderedDict()
+
+        node = head
+        while node is not None:
+            d[node.val] = True
+            node = node.next
+
+        result = None
+        for key in reversed(d.keys()):
+            node = ListNode(val=key)
+
+            if result is None:
+                result = node
+            else:
+                node.next = result
+                result = node
+
+        return result
 # @lc code=end
 
