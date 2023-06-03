6afc38a62f983b62bf533d800a9ecd1ba8eafc5a
keyem4251 <kisk.emura@gmail.com>
2023-06-03 16:01:44 +0900

    ac solution

--- codes/5.longest-palindromic-substring.py
+++ codes/5.longest-palindromic-substring.py
@@ -8,18 +8,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         left = 0
-        right = 0
+        right = 1
 
         longest_substring = ""
 
         while left < len(s):
             # print(f"s[left:right]: {s[left:right]}, s[left:right][::-1]: {s[left:right][::-1]}, right: {right}, left: {left}")
+            if len(longest_substring) >= len(s):
+                break
+
             if s[left:right] == s[left:right][::-1] and len(longest_substring) < len(s[left:right]):
                 longest_substring = s[left:right]
 
             right += 1
             if right > len(s):
-                right = 0
+                right = left
                 left += 1
 
         return longest_substring
