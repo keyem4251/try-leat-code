9e4ccdb7633c3902201fcef6fdc352c151829f0e
keyem4251 <kisk.emura@gmail.com>
2023-06-08 21:37:43 +0900

    remove col

--- codes/6.zigzag-conversion.py
+++ codes/6.zigzag-conversion.py
@@ -12,29 +12,36 @@ class Solution:
 
         # matrixのindexを行と列を0で設定
         row_i = 0
-        col_i = 0
 
+        # 下りか、上りか
         is_desc = True
 
         # 文字列分ループしてrowsに詰める
-        for c in s:
-            matrix[row_i][col_i].append(c)
-            
-            # 行を1つ進める
+        for c in s:            
+            # 行を進める
             if is_desc:
+                # 下りはそのまま文字列入れる
+                matrix[row_i].append(c)
                 row_i += 1
             else:
+                # 上りは
+                matrix[row_i].append("")
                 row_i -= 1
-                col_i += 1
 
             if row_i == numRows:
                 is_desc = False
-                col_i += 1
                 row_i -= 1
 
             if row_i == 0:
                 is_desc = True
-            
+
+        result = ""
+        for row in matrix:
+            for col in row:
+                if col:
+                    result.append(col)
+
+        return result
 
 # @lc code=end
 
