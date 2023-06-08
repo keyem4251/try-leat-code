663c81b9720c420eda16bd405378039f4cd9fc41
keyem4251 <kisk.emura@gmail.com>
2023-06-08 21:16:11 +0900

    matrix list

--- codes/6.zigzag-conversion.py
+++ codes/6.zigzag-conversion.py
@@ -7,6 +7,34 @@
 # @lc code=start
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        
+        # matrixを作成
+        matrix = [[] for i in range(numRows)]
+
+        # matrixのindexを行と列を0で設定
+        row_i = 0
+        col_i = 0
+
+        is_desc = True
+
+        # 文字列分ループしてrowsに詰める
+        for c in s:
+            matrix[row_i][col_i].append(c)
+            
+            # 行を1つ進める
+            if is_desc:
+                row_i += 1
+            else:
+                row_i -= 1
+                col_i += 1
+
+            if row_i == numRows:
+                is_desc = False
+                col_i += 1
+                row_i -= 1
+
+            if row_i == 0:
+                is_desc = True
+            
+
 # @lc code=end
 
